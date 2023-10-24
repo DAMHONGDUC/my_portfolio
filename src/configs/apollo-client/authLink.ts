d481b314b5ec1342c647a1bdb800dce95661c2c7
Dam Hong Duc <hongduc001h@gmail.com>
2023-10-24 20:43:53 +0700

    feat: remove un-use code

--- src/configs/apollo-client/authLink.ts
+++ src/configs/apollo-client/authLink.ts
@@ -1,12 +1,8 @@
-import { COOKIE_ITEM } from '@/constants'
 import { ApolloLink, DefaultContext } from '@apollo/client'
 import Cookies from 'js-cookie'
 
 export const authLink = new ApolloLink((operation, forward) => {
-  const getToken = {
-    admin: Cookies.get(COOKIE_ITEM.ADMIN_TOKEN),
-    users: Cookies.get(COOKIE_ITEM.USER_TOKEN),
-  }
+  const getToken = {}
   const pathname = window.location.pathname.split('/')[1]
 
   // const token =
